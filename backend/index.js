4f2779e88a83bd4d2212196c660c0b2ce81654dd
FernandoMontenegro <paul03montenegro@gmail.com>
2025-05-27 09:26:58 -0500

    Cambios en index en backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+const libros = [
+  {lib_id: 1, lib_titulo: "Chainsaw-man", lib_autor: "Tatsuki Fujimoto"},
+  {lib_id: 2, lib_titulo: "Berserk", lib_autor: "Kentaro Miura"},
+  {lib_id: 3, lib_titulo: "Monster", lib_autor: "Naoki Urasawa"}
+]
+
 app.use(cors())
 
 app.use(express.json());
@@ -12,6 +18,14 @@ app.use(express.json());
 app.get('/ping', (req, res) => {
     res.json({data: 'Pong - FPMM'});
 });
+
+app.post('/agregar_libros', (req, res) => {
+  const { lib_id, lib_titulo, lib_autor } = req.body;
+  const libro = { lib_id, lib_titulo, lib_autor };
+  libros.push(libro);
+  res.status(201).json(libros);
+});
+
 
 app.get('/examen', (req, res) => {
   res.json({data: 'Hola Mundo!'});
